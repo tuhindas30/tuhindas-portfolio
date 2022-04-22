5d324f2e925d49e559142da302a221004d01bd7d
Tuhin Das <mailme.tuhindas@gmail.com>
2022-04-22 13:02:08 +0530

    📱 style: update responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,44 @@
+import Image from "next/image";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <main className={styles.introHeader}>
-        <div style={{ color: "var(--primary-color)" }}>HEY THERE,</div>
-        <h1>I&apos;m Tuhin 👋</h1>
-        <p style={{ color: "var(--light-offset-color)", lineHeight: "2.1rem" }}>
-          I&apos;m a full stack web developer who likes playing with React a
-          lot. Welcome to my personal slice of the internet. I&apos;m glad you
-          are here!
+    <header className={styles.introHeader}>
+      <div>
+        <p className={styles.greetMessage}>Hey there,</p>
+        <h1>
+          I&apos;m Tuhin&nbsp;
+          <Image
+            src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif"
+            alt="waving hand"
+            width={40}
+            height={40}
+          />
+        </h1>
+        <p className={styles.aboutBrief}>
+          I&apos;m a full-stack web developer who loves playing with&nbsp;
+          <a href="https://reactjs.org" className={styles.inlineLink}>
+            React.js
+          </a>
+          &nbsp;a lot. Welcome to my personal slice of the internet. I&apos;m so
+          glad you are here!&nbsp;
+          <span className={styles.smilingGif}>
+            <Image
+              src="/images/smiling_face.gif"
+              alt="waving hand"
+              width={40}
+              height={40}
+            />
+          </span>
         </p>
         <a
           href="Tuhin_Das_Resume.pdf"
           target="_blank"
           rel="noreferrer"
-          className={styles.primaryButton}>
+          className={`button primary-button ${styles.headerButton}`}>
           Resume
         </a>
-      </main>
+      </div>
     </header>
   );
 };
