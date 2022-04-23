67532240499c20474ea2b4a175170de423cbd80c
Tuhin Das <mailme.tuhindas@gmail.com>
2022-04-23 15:16:52 +0530

    📱 style: update responsiveness

--- src/components/GetInTouch/GetInTouch.tsx
+++ src/components/GetInTouch/GetInTouch.tsx
@@ -1,21 +1,40 @@
+import Image from "next/image";
 import Section from "../Section";
 import styles from "./GetInTouch.module.css";
 
-const GetInTouchSection = () => {
+const GetInTouch = () => {
   return (
     <Section>
-      <h1 className={styles.heading}>Get In Touch!</h1>
-      <p>
+      <h2 className={styles.heading}>
+        Get In Touch!&nbsp;
+        <span className={styles.imageGif}>
+          <Image
+            src="/images/handshake.gif"
+            alt="handshake"
+            width={50}
+            height={50}
+          />
+        </span>
+      </h2>
+      <p className={styles.message}>
         Whether you have an idea for a project or just want to say hi, feel free
-        to shoot me an email!
+        to shoot me an email!&nbsp;
+        <span className={styles.imageGif}>
+          <Image
+            src="/images/dart_hit.gif"
+            alt="Dart hit"
+            width={40}
+            height={40}
+          />
+        </span>
       </p>
       <a
         href="mailto:mailme.tuhindas@gmail.com"
-        className={styles.primaryButton}>
+        className={`button primary-button ${styles.button}`}>
         Say Hello
       </a>
     </Section>
   );
 };
 
-export default GetInTouchSection;
+export default GetInTouch;
